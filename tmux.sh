572ef632013399f3ddb2f7a237cd1ac2c4e0f957
永雏塔菲 <108621198+taffychan@users.noreply.github.com>
2022-07-10 09:01:54 +0800

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -23,7 +23,7 @@ PACKAGE_UPDATE=("apt-get -y update" "apt-get -y update" "yum -y update" "yum -y 
 PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install")
 PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove")
 
-[[ $EUID -ne 0 ]] && red "请在root用户下运行脚本" && exit 1
+[[ $EUID -ne 0 ]] && red "注意：请在root用户下运行脚本" && exit 1
 
 CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
 
@@ -35,7 +35,7 @@ for ((int = 0; int < ${#REGEX[@]}; int++)); do
     [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
 done
 
-[[ -z $SYSTEM ]] && red "不支持当前VPS系统，请使用主流的操作系统" && exit 1
+[[ -z $SYSTEM ]] && red "不支持当前VPS的系统，请使用主流的操作系统" && exit 1
 
 if [[ -z $(type -P tmux) ]]; then
     if [[ ! $SYSTEM == "CentOS" ]]; then
@@ -43,3 +43,32 @@ if [[ -z $(type -P tmux) ]]; then
     fi
     ${PACKAGE_INSTALL[int]} tmux
 fi
+
+menu(){
+    clear
+    echo "#############################################################"
+    echo -e "#                    ${RED} Tmux  后台管理脚本${PLAIN}                    #"
+    echo -e "# ${GREEN}作者${PLAIN}: taffychan                                           #"
+    echo -e "# ${GREEN}GitHub${PLAIN}: https://github.com/taffychan                      #"
+    echo "#############################################################"
+    echo ""
+    echo -e " ${GREEN}1.${PLAIN} 创建Tmux后台会话并设置会话名称"
+    echo " -------------"
+    echo -e " ${GREEN}2.${PLAIN} 查看并进入指定Tmux后台会话"
+    echo -e " ${GREEN}3.${PLAIN} 查看并删除指定Tmux后台会话"
+    echo " -------------"
+    echo -e " ${GREEN}0.${PLAIN} 退出脚本"
+    echo ""
+    yellow "使用脚本的一些小提示："
+    yellow "1. 退出Screen后台会话时，请按Ctrl+B+D快捷键退出"
+    echo ""
+    read -rp "请输入选项 [0-4]:" menuNumberInput
+    case "$menuNumberInput" in 
+        1 ) createTmuxSession ;;
+        2 ) enterTmuxSession ;;
+        3 ) deleteTmuxSession ;;
+        * ) exit 1 ;;
+    esac
+}
+
+menu
